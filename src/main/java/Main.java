f08000f2734aa3bab8bbb7abff22ead51ca33401
DongvinPark <dongvin99@naver.com>
2025-12-30 14:36:14 +0900

    implemented python script runner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import index.KR.interest.BaseInterestRate;
 import index.KR.interest.BondRate10Year;
 import index.KR.interest.BondRate3Year;
 import index.KR.raw_materials.KrxGold1KG;
+import index.KR.stock.Kosdaq;
+import index.KR.stock.Kospi;
 import index.US.interest.FederalFundsRate;
 import index.US.interest.UsBondRate10Year;
 import index.US.interest.UsBondRate3Year;
@@ -10,108 +12,127 @@ import index.US.nasdaq_compo.Nasdaq;
 import index.US.origin_bitcoin.BitcoinOriginal;
 import index.US.raw_materials.ComexGoldFuture;
 import index.US.snp500.SnP500;
+import script_for_raw_data_api.PythonScriptRunner;
+import utils.Logger;
 
+import java.util.List;
+
+// .xls 파일과 .xlsx 파일 같은 엑셀파일들은 .csv로 변환 후 처리한다.
 public class Main {
   public static void main(String[] args){
 
-    // .xls 파일과 .xlsx 파일 같은 엑셀파일들은 .csv로 변환 후 처리한다.
-    /*
+    PythonScriptRunner scriptRunner = new PythonScriptRunner(
+        "C:\\dev\\Macro-Economy-Board\\raw-data",
+        List.of(
+            "01-kospi-test.py",
+            "02-kosdaq-test.py",
+            "03-btc-test.py",
+            "04-snp500-test.py",
+            "05-nasdaq-test.py",
+            "06-fred-interest-rates.py",
+            "07-comex-gold-future.py"
+        )
+    );
+    boolean dataFetchingResult = scriptRunner.runPythonDataFetchingScrips();
+    Logger.warn("API Fetching result : " + dataFetchingResult);
+
     Kospi kospi = new Kospi();
-    kospi.convertToJson(
+    boolean kospiResult = kospi.convertToJson(
         "C:\\dev\\Macro-Economy-Board\\raw-data\\kospi-to-20250624.csv",
         "C:\\dev\\Macro-Economy-Board"
-    );*/
+    );
+    Logger.warn("KOSPI result : " + kospiResult);
 
-    /*
     Kosdaq kosdaq = new Kosdaq();
-    kosdaq.convertToJson(
+    boolean kosdaqResult = kosdaq.convertToJson(
         "C:\\dev\\Macro-Economy-Board\\raw-data\\kosdaq-to-20250624.csv",
         "C:\\dev\\Macro-Economy-Board"
-    );*/
+    );
+    Logger.warn("KOSDAQ result : " + kosdaqResult);
 
-    /*
-    BondRate3Year bondRate3Year = new BondRate3Year();
-    bondRate3Year.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\국고채3년이자율2022-20250623.xls",
-        "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
-    BondRate10Year bondRate10Year = new BondRate10Year();
-    bondRate10Year.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\국고채10년이자율2020-20250623.xls",
-        "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
-    WonDollar wonDollar = new WonDollar();
-    wonDollar.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\원_달러 환율 및 원화 명목실효환율 지수.xlsx",
-        "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
     BaseInterestRate baseInterestRate = new BaseInterestRate();
-    baseInterestRate.convertToJson(
+    boolean krBaseRateResult = baseInterestRate.convertToJson(
         "C:\\dev\\Macro-Economy-Board\\data-batch-updater\\meboard-updater\\kr-base-interest-rate-record.csv",
         "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
+    );
+    Logger.warn("KR Base Rate result : " + krBaseRateResult);
 
-    /*
-    BitcoinOriginal bitcoinOriginal = new BitcoinOriginal();
-    bitcoinOriginal.convertToJson(
-            "C:\\dev\\Macro-Economy-Board\\raw-data\\btc_daily_data.csv",
-            "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
-    SnP500 snP500 = new SnP500();
-    snP500.convertToJson(
-            "C:\\dev\\Macro-Economy-Board\\raw-data\\snp500_daily_data.csv",
-            "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
-    Nasdaq nasdaq = new Nasdaq();
-    nasdaq.convertToJson(
-            "C:\\dev\\Macro-Economy-Board\\raw-data\\nasdaq_daily_data.csv",
-            "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
-
-    /*
-    FederalFundsRate federalFundsRate = new FederalFundsRate();
-    federalFundsRate.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_fed_rate.csv",
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\"
-    );*/
-
-    /*
-    UsBondRate3Year usBondRate3Year = new UsBondRate3Year();
-    usBondRate3Year.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_treasury_3y.csv",
+    BondRate3Year bondRate3Year = new BondRate3Year();
+    boolean krBond3yResult = bondRate3Year.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\국고채3년이자율2022-20250623.xls",
         "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
+    );
+    Logger.warn("KR Bond 3Y result : " + krBond3yResult);
 
-    /*
-    UsBondRate10Year usBondRate10Year = new UsBondRate10Year();
-    usBondRate10Year.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_treasury_10y.csv",
+    BondRate10Year bondRate10Year = new BondRate10Year();
+    boolean krBond10yResult = bondRate10Year.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\국고채10년이자율2020-20250623.xls",
         "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
+    );
+    Logger.warn("KR Bond 10Y result : " + krBond10yResult);
 
-    /*
-    ComexGoldFuture comexGoldFuture = new ComexGoldFuture();
-    comexGoldFuture.convertToJson(
-        "C:\\dev\\Macro-Economy-Board\\raw-data\\gold_future_comex.csv",
+    WonDollar wonDollar = new WonDollar();
+    boolean wonDollarResult = wonDollar.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\원_달러 환율 및 원화 명목실효환율 지수.xlsx",
         "C:\\dev\\Macro-Economy-Board\\raw-data"
-    );*/
+    );
+    Logger.warn("KR Won/Dollar Exchange Rate result : " + wonDollarResult);
 
-    /*
     KrxGold1KG krxGold1KG = new KrxGold1KG();
-    krxGold1KG.convertToJson(
+    boolean krGold1KgResult = krxGold1KG.convertToJson(
         "C:\\dev\\Macro-Economy-Board\\raw-data\\krx-gold-2024-2025.csv",
         "C:\\dev\\Macro-Economy-Board\\raw-data\\"
-    );*/
+    );
+    Logger.warn("KRX Gold 1KG Spot result : " + krGold1KgResult);
+
+    SnP500 snP500 = new SnP500();
+    boolean snP500Result = snP500.convertToJson(
+            "C:\\dev\\Macro-Economy-Board\\raw-data\\snp500_daily_data.csv",
+            "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("S&P 500 result : " + snP500Result);
+
+    Nasdaq nasdaq = new Nasdaq();
+    boolean nasdaqResult = nasdaq.convertToJson(
+            "C:\\dev\\Macro-Economy-Board\\raw-data\\nasdaq_daily_data.csv",
+            "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("NASDAQ Composite result : " + nasdaqResult);
+
+    FederalFundsRate federalFundsRate = new FederalFundsRate();
+    boolean usBaseRateResult = federalFundsRate.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_fed_rate.csv",
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\"
+    );
+    Logger.warn("US Base Rate result : " + usBaseRateResult);
+
+    UsBondRate3Year usBondRate3Year = new UsBondRate3Year();
+    boolean usBond3yResult = usBondRate3Year.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_treasury_3y.csv",
+        "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("US Bond 3Y result : " + usBond3yResult);
+
+    UsBondRate10Year usBondRate10Year = new UsBondRate10Year();
+    boolean usBond10yResult = usBondRate10Year.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\us_treasury_10y.csv",
+        "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("US Bond 10Y result : " + usBond10yResult);
+
+    BitcoinOriginal bitcoinOriginal = new BitcoinOriginal();
+    boolean btcResult = bitcoinOriginal.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\btc_daily_data.csv",
+        "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("BTC Bitcoin result : " + btcResult);
+
+    ComexGoldFuture comexGoldFuture = new ComexGoldFuture();
+    boolean comexGoldResult = comexGoldFuture.convertToJson(
+        "C:\\dev\\Macro-Economy-Board\\raw-data\\gold_future_comex.csv",
+        "C:\\dev\\Macro-Economy-Board\\raw-data"
+    );
+    Logger.warn("COMEX Gold Future result : " + comexGoldResult);
 
   }// end of main
 }// Main class
---
+++ src/main/java/script_for_raw_data_api/PythonScriptRunner.java
@@ -0,0 +1,62 @@
+package script_for_raw_data_api;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static constants.Constant.KOSDAQ_JSON_SUFFIX;
+import static constants.Constant.KOSPI_JSON_SUFFIX;
+
+public class PythonScriptRunner {
+
+  private final String scriptRootDir;
+  private final List<String> scriptFileNameList;
+
+  public PythonScriptRunner(
+      String scriptRootDir,
+      List<String> fileNameList
+  ) {
+    this.scriptRootDir = scriptRootDir;
+    this.scriptFileNameList = fileNameList;
+  }
+
+  public boolean runPythonDataFetchingScrips(){
+    try {
+      String pythonVersion;
+      for(String scriptName : scriptFileNameList){
+        if(scriptName.contains(KOSPI_JSON_SUFFIX) || scriptName.contains(KOSDAQ_JSON_SUFFIX)){
+          pythonVersion = "python3";
+        } else {
+          pythonVersion = "python";
+        }
+
+        String scriptFileDir = scriptRootDir + File.separator + scriptName;
+        ProcessBuilder pb = new ProcessBuilder(
+            pythonVersion,
+            scriptFileDir
+        );
+
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+
+        try (BufferedReader reader =
+                 new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+          String line;
+          while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+          }
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exit code for " + scriptName + " : " + exitCode);
+      }//for
+
+      return true;
+    } catch (Exception e){
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+}
